e840358b5176ea4186e65ae178bf156e53687b17
naveedprojects <103450750+naveedprojects@users.noreply.github.com>
2024-03-17 16:38:13 +0500

    Add files via upload

---
+++ capture-videos.py
@@ -0,0 +1,67 @@
+import os
+import cv2
+import uuid
+from datetime import datetime
+import threading
+
+def record_video(camera_index, output_directory="video0"):
+    # Get MAC address
+    mac_address = hex(uuid.getnode())[2:]
+
+    # Check if output directory exists, create if not
+    if not os.path.exists(output_directory):
+        os.makedirs(output_directory)
+
+    # Capture video from camera
+    cap = cv2.VideoCapture(camera_index)
+
+    width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+    height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+
+    # Define the codec and create VideoWriter object
+    fourcc = cv2.VideoWriter_fourcc(*'mp4v')
+    output_filename = os.path.join(output_directory, f"{mac_address}-{datetime.now().strftime('%Y%m%d-%H%M%S')}.mp4")
+    out = cv2.VideoWriter(output_filename, fourcc, 20.0, (width, height))
+    out_is_open = True
+
+    while(cap.isOpened()):
+        ret, frame = cap.read()
+        if ret == True:
+            if frame.mean() > 50:
+                output_filename = os.path.join(output_directory, f"{mac_address}-{datetime.now().strftime('%Y%m%d-%H%M%S')}.mp4")
+                out = cv2.VideoWriter(output_filename, fourcc, 20.0, (width, height))
+            elif out_is_open:
+                out.release()
+                out_is_open = False
+
+            out.write(frame)
+            cv2.imshow('frame', frame)
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+                break
+        else:
+            break
+
+    # Release resources
+    cap.release()
+    out.release()
+    cv2.destroyAllWindows()
+
+def main():
+    # Define camera indices
+    camera1_index = 0  # Index of the first camera
+    camera2_index = 1  # Index of the second camera
+
+    # Create threads for each camera
+    thread1 = threading.Thread(target=record_video, args=(camera1_index, "video0",))
+    thread2 = threading.Thread(target=record_video, args=(camera2_index, "video1", ))
+
+    # Start the threads
+    thread1.start()
+    thread2.start()
+
+    # Wait for both threads to finish
+    thread1.join()
+    thread2.join()
+
+if __name__ == "__main__":
+    main()
